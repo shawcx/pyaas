eaf20f1ceddedb71bc66b95e46a0684992bddddd
moertle <moertle@gmail.com>
2014-04-29 17:39:34 -0400

    push to pypi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ zf.close()
 
 setup(
     name = 'pyaas',
-    version = '0.3.2',
+    version = '0.3.3',
     author = 'Matthew Oertle',
     author_email = 'moertle@gmail.com',
     url = 'https://github.com/moertle/pyaas',
