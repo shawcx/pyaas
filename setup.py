ab71a01764358825f40c965d8aadc84b9e9a62b2
Matthew Oertle <moertle@gmail.com>
2014-08-13 13:08:53 -0400

    updated PyPi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ zf.close()
 
 setup(
     name = 'pyaas',
-    version = '0.3.5',
+    version = '0.3.6',
     author = 'Matthew Oertle',
     author_email = 'moertle@gmail.com',
     url = 'https://github.com/moertle/pyaas',
