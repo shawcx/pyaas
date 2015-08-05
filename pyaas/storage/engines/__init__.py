4d7ecfc820eb6b0e0806f4a9339ed57623836fbe
Mark Riley <mark@eucleo.com>
2015-08-05 13:17:57 -0400

    databases may be added manually.

--- pyaas/storage/engines/__init__.py
+++ pyaas/storage/engines/__init__.py
@@ -3,6 +3,7 @@ import logging
 
 import pyaas
 
+
 class StorageModule(pyaas.module.PyaasModule):
     PKG_PATH = 'pyaas.storage.engines'
 
@@ -14,41 +15,52 @@ class StorageModule(pyaas.module.PyaasModule):
         instances = pyaas.config.get('modules', 'storage')
         instances = [instance.strip() for instance in instances.split(',')]
         for instance in instances:
-            logging.info('Loading storage instance: %s', instance)
-
-            if not pyaas.config.has_section(instance):
-                logging.warn('No configuration for instance: %s', instance)
-                continue
-
-            if not pyaas.config.has_option(instance, 'engine'):
-                logging.warn('No engine specified for instance: %s', instance)
-                continue
-
-            params = dict(pyaas.config.items(instance))
-
-            engine = params.pop('engine')
-
-            engineClass = cls.loadModule(engine)
-
-            # convert user supplied engine name into a single value
-            section = engine.split('.')[-1]
-
-            if pyaas.config.has_section(section):
-                options = dict(pyaas.config.items(section))
-            else:
-                options = {}
-
-            options.update(params)
-
-            isCritical = options.pop('critical', 'true')
-            try:
-                pyaas.storage.databases[instance] = engineClass(**options)
-                pyaas.storage.databases[instance].Initialize()
-            except pyaas.error as e:
-                if isCritical.lower() in ['false', 'f' 'no', 'n', '0']:
-                    logging.warn('Unable to instantiate engine: %s:%s', engine, instance)
-                    logging.debug('%s', e)
-                else:
-                    raise
-
+            cls.loadInstance(instance)
         return
+
+    @classmethod
+    def loadInstance(cls, instance, instance_dup=None, **kwargs):
+        logging.info('Loading storage instance: %s', instance)
+        conf = pyaas.config
+
+        if 'engine' in kwargs:
+            engine = kwargs.pop('engine')
+        elif conf.has_section(instance) and conf.has_option(instance, 'engine'):
+            engine = pyaas.config.get(instance, 'engine')
+        elif instance_dup is not None and conf.has_section(instance_dup) and conf.has_option(instance_dup, 'engine'):
+            engine = pyaas.config.get(instance_dup, 'engine')
+        else:
+            logging.warn('No configuration for instance: %s', instance)
+            return None
+
+        engine_class = cls.loadModule(engine)
+
+        # convert user supplied engine name into a single value
+        engine_defaults = engine.split('.')[-1]
+
+        # cascade settings
+        options = {}
+        if conf.has_section(engine_defaults):
+            options.update(dict(conf.items(engine_defaults)))
+        if instance_dup is not None and conf.has_section(instance_dup):
+            options.update(dict(conf.items(instance_dup)))
+        if conf.has_section(instance):
+            options.update(dict(conf.items(instance)))
+        options.update(kwargs)
+
+        options.pop('engine')
+        critical = options.pop('critical', 'true')
+        try:
+            opts = dict(options)
+            opts['password'] = '****'
+            logging.debug('Loading storage instance %s with args %s', instance, opts)
+            pyaas.storage.databases[instance] = db = engine_class(**options)
+            db.Initialize()
+            return db
+        except pyaas.error as e:
+            if critical.lower() in ['false', 'f' 'no', 'n', '0']:
+                logging.warn('Unable to instantiate engine: %s:%s', engine, instance)
+                logging.debug('%s', e)
+            else:
+                raise
+        return None
